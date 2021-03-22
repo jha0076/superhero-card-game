ed3670eae6a9b2aa26678c10e61f18fd8a35b5a8
Kumar Satyam <71371931+jha0076@users.noreply.github.com>
2021-03-23 00:05:34 +0530

    Add files via upload

---
+++ superhero.py
@@ -0,0 +1,22 @@
+# import modules
+import itertools, random
+
+# make a deck of cards
+deck = list(itertools.product(range(1,14),['Ant-Man',
+'Aquaman',
+'Asterix',
+'The Atom',
+'The Avengers',
+'Batgirl',
+'Batman',
+'Batwoman',
+'Black Canary'
+]))
+
+# shuffle the cards
+random.shuffle(deck)
+
+# draw five cards
+print("You got:")
+for i in range(5):
+   print(deck[i][0], "of", deck[i][1])
